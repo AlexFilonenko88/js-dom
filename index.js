e3c1a402d95a1f395a35e1a10284cec17a3918f3
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2022-10-06 17:13:51 +0300

    add task

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const addButton = document.querySelector('.button1');
 const deleteButton = document.querySelector('.button4');
 const rowHeads = document.querySelectorAll('.wrapper-table th');
+const inputFilter = document.querySelector('.button3');
 
-let data = [];
+let data = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : [];
 let checkedIds = [];
-
 let sortDirection = 1;
 
-const renderTable = () => {
+const renderTable = (tableData) => {
     const tableBody = document.querySelector('.wrapper-table tbody');
     tableBody.innerHTML = '';
 
-    data.forEach(item => {
+    tableData.forEach(item => {
         const tr = document.createElement('tr');
         for (let field in item) { // толкь ключ
             const td = document.createElement('td');
@@ -22,7 +22,7 @@ const renderTable = () => {
         const checkInput = document.createElement('input')
         checkInput.setAttribute('type', 'checkbox')
         checkInput.addEventListener('change', (e) => {
-            if(e.target.checked){
+            if (e.target.checked) {
                 checkedIds.push(item.id);
             } else {
                 checkedIds = checkedIds.filter(id => id !== item.id);
@@ -36,6 +36,10 @@ const renderTable = () => {
     })
 }
 
+if(data.length){
+    renderTable(data);
+};
+
 const handleAddDate = () => {
     const inputCollection = document.querySelectorAll(".wrapper-input > input");
     const newData = {};
@@ -44,25 +48,28 @@ const handleAddDate = () => {
     for (let i = 0; i < inputCollection.length; i++) {
         newData[inputCollection[i].name] = inputCollection[i].value;
     }
-    console.log(newData);
 
     data.push(newData);
-    renderTable();
+
+    renderTable(data);
+
+    localStorage.setItem('data', JSON.stringify(data));
 }
 
 addButton.addEventListener('click', handleAddDate);
 
-deleteButton.addEventListener('click', ()=>{
+deleteButton.addEventListener('click', () => {
     data = data.filter(el => !checkedIds.includes(el.id));
     checkedIds = [];
-    renderTable();
+
+    renderTable(data);
 })
 
-rowHeads.forEach(el => el.addEventListener('click', ()=>{
-    data.sort((a,b)=>{
-        if(a[el.textContent] > b[el.textContent]){
+rowHeads.forEach(el => el.addEventListener('click', () => {
+    data.sort((a, b) => {
+        if (a[el.textContent] > b[el.textContent]) {
             return -1 * sortDirection;
-        }else if ( a[el.textContent] < b[el.textContent]) {
+        } else if (a[el.textContent] < b[el.textContent]) {
             return 1 * sortDirection;
         } else {
             return 0;
@@ -71,5 +78,19 @@ rowHeads.forEach(el => el.addEventListener('click', ()=>{
 
     sortDirection = sortDirection === 1 ? -1 : 1;
 
-    renderTable();
+    renderTable(data);
 }));
+
+const handleFindData = (e) => {
+    renderTable(data.filter(el => {
+        for (let field in el) {
+            if (typeof el[field] === 'string') {
+                if (el[field].includes(e.target.value)) {
+                    return true;
+                }
+            }
+        }
+    }));
+}
+
+inputFilter.addEventListener('input', handleFindData);
