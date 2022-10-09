bb76857f4dd0153c0e9e858cd7be0da8f4c63314
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2022-10-09 12:29:36 +0300

    закончили функционал

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const addButton = document.querySelector('.button1');
 const deleteButton = document.querySelector('.button4');
 const rowHeads = document.querySelectorAll('.wrapper-table th');
 const inputFilter = document.querySelector('.button3');
+const buttonEdit = document.querySelector('.button2');
+const buttonEditData = document.querySelector('.button-edit');
 
 let data = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : [];
-let checkedIds = [];
+let checkedIds = 0;
 let sortDirection = 1;
 
 const renderTable = (tableData) => {
@@ -22,12 +24,18 @@ const renderTable = (tableData) => {
         const checkInput = document.createElement('input')
         checkInput.setAttribute('type', 'checkbox')
         checkInput.addEventListener('change', (e) => {
-            if (e.target.checked) {
-                checkedIds.push(item.id);
+            const value = e.target.checked;
+            const checkboxAll = document.querySelectorAll('input[type=checkbox]');
+            checkboxAll.forEach(checkbox => {
+                checkbox.checked = false
+            })
+            if (value) {
+                checkedIds = item.id;
+                e.target.checked = true;
             } else {
-                checkedIds = checkedIds.filter(id => id !== item.id);
+                e.target.checked = false;
+                checkedIds = 0;
             }
-
             console.log(checkedIds);
         })
         checkTd.append(checkInput)
@@ -36,9 +44,10 @@ const renderTable = (tableData) => {
     })
 }
 
-if(data.length){
+if (data.length) {
     renderTable(data);
-};
+}
+;
 
 const handleAddDate = () => {
     const inputCollection = document.querySelectorAll(".wrapper-input > input");
@@ -59,8 +68,8 @@ const handleAddDate = () => {
 addButton.addEventListener('click', handleAddDate);
 
 deleteButton.addEventListener('click', () => {
-    data = data.filter(el => !checkedIds.includes(el.id));
-    checkedIds = [];
+    data = data.filter(el => checkedIds !== el.id);
+    checkedIds = 0;
 
     renderTable(data);
 })
@@ -94,3 +103,45 @@ const handleFindData = (e) => {
 }
 
 inputFilter.addEventListener('input', handleFindData);
+
+const onShowEdit = () => {
+    const editForm = document.querySelector('.wrapper-input.edit');
+    const inputs = editForm.querySelectorAll('input');
+
+    if (checkedIds) {
+        editForm.style.display = 'block';
+        const checkedData = data.find(el => el.id === checkedIds);
+        inputs.forEach(el => el.value = checkedData[el.name]);
+    }
+}
+
+buttonEdit.addEventListener('click', onShowEdit);
+
+const handleEditDate = () => {
+    const inputCollection = document.querySelectorAll(".wrapper-input.edit > input");
+    const newData = {};
+    newData.id = checkedIds;
+
+    for (let i = 0; i < inputCollection.length; i++) {
+        newData[inputCollection[i].name] = inputCollection[i].value;
+    }
+
+    data = data.map(el => {
+        if(el.id === checkedIds){
+            return newData;
+        }
+        return el;
+    });
+
+    renderTable(data);
+
+    localStorage.setItem('data', JSON.stringify(data));
+
+
+
+    const editForm = document.querySelector('.wrapper-input.edit');
+
+    editForm.style.display = 'none';
+}
+
+buttonEditData.addEventListener('click', handleEditDate);
